7a2e4bbe8612d6596bc7e1834b3c7a841268a51f
İhsan Özdemir <ihsannozdemiro1@gmail.com>
2026-08-06 21:33:09 +0300

    program cs guncellendi

--- BaglantiApp/Program.cs
+++ BaglantiApp/Program.cs
@@ -1,21 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
-
 app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "BaglantiApp API v1");
-    c.RoutePrefix = "swagger";
-});
-
+app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "BaglantiApp API v1"); c.RoutePrefix = "swagger"; });
 app.MapGet("/", () => "BaglantiApp API Canli Ortamda Calisiyor!");
-
 app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
