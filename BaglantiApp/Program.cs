af7f17bcfb63af376d5f19eebf6b85e6d48f8b99
İhsan Özdemir <ihsannozdemiro1@gmail.com>
2026-08-06 21:29:18 +0300

    ana sayfa endpointi ve swagger duzeltildi

--- BaglantiApp/Program.cs
+++ BaglantiApp/Program.cs
@@ -1,37 +1,20 @@
-using BaglantiApp.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Veritabanı Servisi (SqlServer)
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-    });
-});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-
-{
-    app.UseSwagger();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "BaglantiApp API v1");
-    c.RoutePrefix = string.Empty; // Swagger'ı doğrudan ana sayfaya (https://baglanti-app.onrender.com/) bağlar
-});s
-    app.UseSwaggerUI();
-}
+    c.RoutePrefix = "swagger";
+});
 
-app.UseHttpsRedirection();
-app.UseCors("AllowAll");
-app.UseStaticFiles();
+app.MapGet("/", () => "BaglantiApp API Canli Ortamda Calisiyor!");
+
 app.UseAuthorization();
 app.MapControllers();
 
